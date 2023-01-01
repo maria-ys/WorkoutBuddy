3ed412718e01c2617ddef99dbabd8aae8a475caa
maria-ys <ys.mariayounas@gmail.com>
2023-01-01 15:59:49 +0500

    dotenv setup

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
+require('dotenv').config()
 const express = require('express');
 
 // express app
 const app = express();
 
+// middleware
+app.use((req,res,next)=>{
+    console.log(req.url, req.method);
+    next();
+})
+
 // routes 
 app.get('/', (req, res)=>{
     res.json({"mssg":"Welcome to the app"});
 })
+
 // listening requests 
-app.listen(3000, ()=>{
-    console.log("listening for requests at port 3000");
+app.listen(process.env.PORT, ()=>{
+    console.log("listening for requests at port", process.env.PORT);
 })
