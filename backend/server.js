1ccb1c1aa63fc0cb1af4b44f2b402ae660894efd
maria-ys <ys.mariayounas@gmail.com>
2023-01-01 15:44:32 +0500

    server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const express = require('express');
 // express app
 const app = express();
 
+// routes 
+app.get('/', (req, res)=>{
+    res.json({"mssg":"Welcome to the app"});
+})
 // listening requests 
 app.listen(3000, ()=>{
     console.log("listening for requests at port 3000");
